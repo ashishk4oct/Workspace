512790543628cd84de239a12ad0b661e8b6f2078
Kumar, Ashish2 <Ashish2.Kumar@misys.com>
2018-07-02 13:32:49 +0530

    comments

--- Java/src/com/java/designpattern/creational/builder/Computer.java
+++ Java/src/com/java/designpattern/creational/builder/Computer.java
@@ -2,6 +2,22 @@ package com.java.designpattern.creational.builder;
 
 public class Computer {
 
+
+/*    
+   1. First of all you need to create a static nested class and then copy all the arguments from the outer 
+   	  class to the Builder class. We should follow the naming convention and if the class name is Computer 
+   	  then builder class should be named as ComputerBuilder.
+   	  
+   2. Java Builder class should have a public constructor with all the required attributes as parameters.
+   
+   3. Java Builder class should have methods to set the optional parameters and it should return the same 
+   	  Builder object after setting the optional attribute.
+   
+   4. The final step is to provide a build() method in the builder class that will return the Object needed 
+   	  by client program. For this we need to have a private constructor in the Class with Builder class as argument.
+*/
+	
+	
 		//required parameters
 		private String HDD;
 		private String RAM;
