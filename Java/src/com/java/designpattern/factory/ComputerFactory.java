8ef91012bb360e480d07f09f6e7838197b8a3a63
Kumar, Ashish2 <Ashish2.Kumar@misys.com>
2018-07-02 13:12:32 +0530

    Abstract Factory Design Pattern

---
+++ Java/src/com/java/designpattern/factory/ComputerFactory.java
@@ -0,0 +1,18 @@
+package com.java.designpattern.factory;
+
+public class ComputerFactory {
+
+	private ComputerFactory() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static Computer getComputer(String type, String ram, String hdd, String cpu) {
+
+		if ("PC".equalsIgnoreCase(type))
+			return new PC(ram, hdd, cpu);
+		else if ("Server".equalsIgnoreCase(type))
+			return new Server(ram, hdd, cpu);
+
+		return null;
+	}
+}
